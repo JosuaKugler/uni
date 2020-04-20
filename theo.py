f3f461a8509d350267f4afc50e47c7df3e871063
JosuaKugler <josua.kugler@gmail.com>
2020-04-20 09:58:12 +0200

    fixed theo

--- theo.py
+++ theo.py
@@ -14,11 +14,11 @@ scrapeurl = 'https://uebungen.physik.uni-heidelberg.de/vorlesung/20201/ptp2'
 s = requests.Session()
 #if cookie is expired:
 
-#r = s.post(url, data=payload)
-#cookie = {'PHPSESSID': requests.utils.dict_from_cookiejar(s.cookies)['PHPSESSID']}
+r = s.post(url, data=payload)
+cookie = {'PHPSESSID': requests.utils.dict_from_cookiejar(s.cookies)['PHPSESSID']}
 
 #else use this cookie:
-cookie = {'PHPSESSID': 'd2jrecj4g5fhshv4pd542b6vd6'}
+#cookie = {'PHPSESSID': 'd2jrecj4g5fhshv4pd542b6vd6'}
 print(cookie)
 
 theo1 = s.get(scrapeurl, cookies = cookie)
