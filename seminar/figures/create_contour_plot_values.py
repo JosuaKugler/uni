2137fa43c8cdfb2379c7fb2d26ac4944c8850c33
JosuaKugler <josua.kugler@gmail.com>
2020-10-16 11:48:26 +0200

    tikz_externalized works

---
+++ seminar/figures/create_contour_plot_values.py
@@ -0,0 +1,56 @@
+import mpmath
+import numpy
+import colorsys
+import matplotlib.pyplot as plt
+import tikzplotlib
+
+# 2-d array of points
+real_points = 200
+imag_points = 200
+points = numpy.zeros((real_points, imag_points), dtype='complex')
+
+
+# viewport on complex plane
+rmin = 1.01
+rmax = 21.01
+imin = -10
+imax = 10
+
+# calculate zeta(s)
+
+for rp in range(real_points):
+    for ip in range(imag_points):
+        try:
+            real = rmin + ((rmax - rmin) * rp/real_points)
+            imag = imin + ((imax - imin) * ip/imag_points)
+            points[ip,rp] = mpmath.zeta(complex(real,imag))
+        except ValueError:
+            # ignore pole
+            pass
+        pass
+    pass
+
+# vectorise abs and log
+abspoints  = numpy.log(numpy.abs( points), dtype='float64')
+realpoints = numpy.log(numpy.real(points), dtype='float64')
+imagpoints = numpy.log(numpy.imag(points), dtype='float64')
+
+fig1 = plt.figure(figsize = (15,15))
+plt.contour(abspoints, 100, linewidths=0.5, origin='lower', extent=[rmin, rmax, imin, imax])
+tikzplotlib.save("figures/half_absolute_value_contour.tex")
+
+
+fig2 = plt.figure(figsize = (15,15))
+plt.contour(realpoints, 100, linewidths=0.5, origin='lower', extent=[rmin, rmax, imin, imax])
+tikzplotlib.save("figures/half_real_value_contour.tex")
+
+#plt.show()
+#fig2.savefig("figures/real_value_contour_plot.pdf")
+
+
+#fig3 = plt.figure(figsize = (15,15))
+#plt.contour(imagpoints, 100, linewidths=0.5, origin='lower', extent=[rmin, rmax, imin, imax])
+#tikzplotlib.save("figures/half_imag_value_contour.tex")
+#plt.show()
+#fig3.savefig("figures/imag_value_contour_plot.pdf")
+
