e12409224274f2b4f7e90991d6e79088a6385e18
JosuaKugler <josua.kugler@gmail.com>
2020-10-27 16:10:00 +0100

    saving the current state

--- seminar/figures/cramer.py
+++ seminar/figures/cramer.py
@@ -19,6 +19,6 @@ plt.step(primes[1:], primediffs)
 y1 = np.power(primes, 0.525)
 y2 = np.sqrt(primes) * np.log(primes)
 plt.plot(primes, y1, "y")
-#plt.plot(primes, y2, "g")
+plt.plot(primes, y2, "g")
 
 plt.show()
