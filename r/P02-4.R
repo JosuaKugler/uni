63461b2e565ad65ac64ac5f9053553b79e7f04e0
JosuaKugler <josua.kugler@gmail.com>
2021-04-28 11:07:00 +0200

    add new R Vl stuff and rename la skript

--- r/P02-4.R
+++ r/P02-4.R
@@ -7,11 +7,15 @@ collatz <- function(x, max_iter) {
   seq <- c(x)
   len <- 1
   while (len < max_iter && x != 1) {
-    if (x %% 2 == 0) x = as.integer(x/2) else x = 3*x + 1
-    len <- len + 1    
+    if (x %% 2 == 0)
+      x = as.integer(x / 2)
+    else
+      x = 3 * x + 1
+    len <- len + 1
     seq[len] <- x
   }
-  if (len == max_iter) len <- NA_integer_
+  if (len == max_iter)
+    len <- NA_integer_
   res = list(seq = seq, len = len)
   res
 }
